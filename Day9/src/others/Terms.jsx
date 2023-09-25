6751d80265e5b094f50d8e1d0b97a73d55a54f72
pujas19 <110535076+pujas19@users.noreply.github.com>
2023-09-25 12:00:45 +0530

    Add files via upload

---
+++ Day9/src/others/Terms.jsx
@@ -0,0 +1,168 @@
+import React from 'react';
+
+const Terms = () => {
+  return (
+    <div className="bg-gray-100 min-h-screen p-4">
+      <div className="max-w-3xl mx-auto bg-white p-6 shadow-md rounded-lg">
+        <h1 className="text-3xl font-semibold mb-4">Terms & Conditions</h1>
+        <p className="mb-4">
+        Innovative Retail Concepts Pvt Ltd (“IRCPL”) is the licensed owner of the brand bigbasket.com and the website bigbasket.com (”The Site”) from Supermarket Grocery Supplies Pvt Ltd (“SGSPL”). As a visitor to the Site/ Customer you are advised to please read the Term & Conditions carefully. By accessing the services provided by the Site you agree to the terms provided in this Terms & Conditions document.
+        </p>
+
+
+        {/* Add more content as needed */}
+
+        <h4 className="text-xl font-bold mt-4 mb-2">Personal Information</h4>
+        <p className="mb-4">
+        As part of the registration process on the Site, IRCPL may collect the following personally identifiable information about you: Name including first and last name, alternate email address, mobile phone number and contact details, Postal code, Demographic profile (like your age, gender, occupation, education, address etc.) and information about the pages on the site you visit/access, the links you click on the site, the number of times you access the page and any such browsing information.
+       </p>
+        <h5 className="text-xl font-bold mt-4 mb-2">Eligibility</h5>
+       <li className="mb-2">
+       Services of the Site would be available to only select geographies in India. Persons who are "incompetent to contract" within the meaning of the Indian Contract Act, 1872 including un-discharged insolvents etc. are not eligible to use the Site. If you are a minor i.e. under the age of 18 years but at least 13 years of age you may use the Site only under the supervision of a parent or legal guardian who agrees to be bound by these Terms of Use. If your age is below 18 years your parents or legal guardians can transact on behalf of you if they are registered users. You are prohibited from purchasing any material which is for adult consumption and the sale of which to minors is prohibited
+       </li>
+        <h5 className="text-xl font-bold mt-4 mb-2">Account & Registration Obligations</h5>
+       <li className="mb-2">
+       All shoppers have to register and login for placing orders on the Site. You have to keep your account and registration details current and correct for communications related to your purchases from the site. By agreeing to the terms and conditions, the shopper agrees to receive promotional communication and newsletters upon registration. The customer can opt out either by unsubscribing in "My Account" or by contacting the customer service. </li>
+        <h5 className="text-xl font-bold mt-4 mb-2">Data Protection Laws</h5>
+       <li className="mb-2">
+       any applicable law for the time being in force relating to the processing of Data, including the Information Technology Act, 2000 and Information Technology (Reasonable Security Practices and Procedures and Sensitive Personal Information) Rules, 2011, as amended or replaced from time to time.
+       </li>
+        <h5 className="text-xl font-bold mt-4 mb-2">Pricing</h5>
+       <li className="mb-2">
+       All the products listed on the Site will be sold at MRP unless otherwise specified. The prices mentioned at the time of ordering will be the prices charged on the date of the delivery. Although prices of most of the products do not fluctuate on a daily basis but some of the commodities and fresh food prices do change on a daily basis. In case the prices are higher or lower on the date of delivery not additional charges will be collected or refunded as the case may be at the time of the delivery of the order.
+       </li>
+        <h5 className="text-xl font-bold mt-4 mb-2">Service Providers</h5>
+       <li className="mb-2">
+       includes entities to whom we, or other Tata Group Entities, will disclose your Data in order for them to process it for the purpose of providing services to us pursuant to written contract.
+       </li>
+        <h5 className="text-xl font-bold mt-4 mb-2">SPDI</h5>
+       <li className="mb-2">
+       the Data Protection Laws define sensitive personal data or information of a person as personal information about that person relating to (a) passwords; (b) financial information such as bank account credit and debit card details or other payment instrument details; (c) physical,Confidential physiological and mental health condition; (d) sexual orientation; (e) medical records and history; or (f) biometric information.
+       </li>
+        <h5 className="text-xl font-bold mt-4 mb-2">IRCPL</h5>
+       <li className="mb-2">
+       IRCPL, a company incorporated in India whose registered office is at Ranka Junction, No. 224 (old Sy No.80/3), 4th Floor, Vijinapura, Old Madras Road, K R Puram, Bangalore - 560016, India
+       </li>
+       <h5 className="text-xl font-bold mt-4 mb-2">Return & Refunds</h5>
+       <li className="mb-5">
+       We have a "no questions asked return and refund policy" which entitles all our members to return the product at the time of delivery if due to some reason they are not satisfied with the quality or freshness of the product. We will take the returned product back with us and issue a credit note for the value of the return products which will be credited to your account on the Site. This can be used to pay your subsequent shopping bills.
+      </li>
+
+        <h4 className="text-xl font-semibold mt-6 mb-4">Governing Law and Jurisdiction</h4>
+        <p className="mb-4">
+        This User Agreement shall be construed in accordance with the applicable laws of India. The Courts at Bangalore shall have exclusive jurisdiction in any proceedings arising out of this agreement.
+</p>
+
+<ul className="list-disc ml-6 mb-4">
+  <li className="mb-2">
+    Contact information: first and last name, email address, postal address, country, employer, phone number, and other similar contact data.
+  </li>
+  <li className="mb-2">
+    Financial information: payment instrument information, transactions, transaction history, preferences, method, mode, and manner of payment, spending pattern or trends, and other similar data.
+  </li>
+  <li className="mb-2">
+    Technical information: website, device, and mobile app usage, Internet Protocol (IP) address, and similar information collected via automated means, such as cookies, pixels, and similar technologies.
+  </li>
+  <li className="mb-2">
+    Transaction information: the date of the transaction, total amount, transaction history, preferences, and related details.
+  </li>
+  <li className="mb-2">
+    Product and service information: Your account membership number, registration and payment information, and program-specific information, when you request products and/or services directly from us or participate in marketing programs.
+  </li>
+  <li className="mb-2">
+    Personal information: Age, sex, date of birth, marital status, nationality, details of government identification documents provided, occupation, ethnicity, religion, travel history, or any other personal information provided in responses to surveys or questionnaires.
+  </li>
+  <li className="mb-2">
+    Your reviews, feedback, and opinions about our products, programs, and services.
+  </li>
+  <li className="mb-2">
+    Loyalty program information: your loyalty membership information, account details, profile or password details, and any frequent flyer or travel partner program affiliation.
+  </li>
+  <li>
+    Payment instrument information – through our application pages, you may choose a 3rd payment service provider of your choice for making payment and may agree to store your payment instrument details with the chosen service provider/s. The payment instrument data is managed by service provider/s.
+  </li>
+</ul>
+        <h4 className="text-xl font-semibold mt-6 mb-2">3. HOW WE COLLECT DATA</h4>
+        <p className="mb-4">We collect Data in the following ways:</p>
+
+        <ol className="list-decimal ml-6 mb-4">
+        <li className="mb-2">
+         Information You Give Us: We receive and store any information you enter on our website or mobile application or give us in any other way (e.g., at outlets, stores, hotels, kiosks). Please see the section titled “Data Shared by You” for more information.
+        </li>
+  <li className="mb-2">
+    Automatic Information We Collect: We use “cookies”, pixels and similar technologies to receive and store certain types of information whenever you interact with us. Please see the section below, titled “Data that is Collected Automatically” for more information.
+  </li>
+  <li className="mb-2">
+    E-mail Communications: To help us make e-mails more relevant and interesting, we often receive a confirmation (if your device supports such capabilities) when you open e-mail from us or click on a link in the e-mail. You can choose not to receive marketing emails from us by clicking on the unsubscribe link in any marketing email.
+  </li>
+  <li className="mb-2">
+    Automatic Information We Collect from Other Websites: We receive and store certain types of information when you interact with third-party websites that use our technology or with whom we have a specific agreement. Because we process this information on behalf of the applicable website operators, collection, processing, and use of such information is subject to the applicable website operators’ privacy policies and is not covered by our Privacy Notice.
+  </li>
+  <li className="mb-2">
+    Information from Other Sources: We may obtain information from other sources. An example of this is when you authorize a third-party website (such as the website of another Tata Group Entity), to interact directly with our website or mobile application to provide or receive Data about you. In that case, we might receive such Data used by that third-party website to identify your account with that website.
+  </li>
+  <li>
+    Information Previously Provided to Tata Group Entities: Where you have shared any information previously with any of the Tata Group Entities and have consented to the further sharing of such information, such information will be shared with us by the Tata Group Entities.
+  </li>
+</ol>
+
+<p>
+  You can make choices about our collection and use of your Data. For example, you may want to access, edit or remove your Data on our website or mobile application. When you are asked to provide Data, you may decline, however, in such a case, you may not be able to avail the benefit of some of our products, programs, or services or access our stores or physical premises.
+</p>
+<h4 className="text-xl font-semibold mt-6 mb-2">4. SHARING OF DATA</h4>
+<p className="mb-4">
+  We may share your Data with/for:
+</p>
+
+<ol className="list-decimal ml-6 mb-4">
+  <li className="mb-2">
+    Partners: We may make available to you services, products, or applications provided by Partners for use on or through our website or mobile application. If you choose to use such a service, customer information related to those transactions may be shared with such Partner.
+    <p className="mt-2">
+      Such Partners will be required to respect the security of your Data and to treat it in accordance with this privacy policy and applicable law.
+    </p>
+  </li>
+
+  <li className="mb-2">
+    Tata Group Entities: We may make available to you products, services, and/or applications of Tata Group Entities to assist them in reaching out to you in relation to their programs or campaigns and to process your queries and requests. Accordingly, we may share your Data with Tata Group Entities. We may also share your Data with the Tata Group Entities as is relevant for the purposes set out in Clause 7 above, and to facilitate the operation of our business.
+  </li>
+
+  <li className="mb-2">
+    Tata Consumer Platform: Your Data may be shared with Tata Group Entities and other participating entities on the Tata Consumer Platform operated by Tata Digital Limited (“TCP”) for purposes of enrollment, offering you products, services, and benefits on the TCP. Accordingly, we may share your Data with other Tata Group Entities, Partners, and Service Providers.
+  </li>
+
+  <li className="mb-2">
+    Service Providers: We or other Tata Group Entities may share your Data with Service Providers. Examples include storing and analyzing Data, protecting and securing our systems, providing search results and links, providing customer service, credit analysis, processing your information for profiling, user analysis, and payment processing.
+    <p className="mt-2">
+      These Service Providers will be required to only process Data in accordance with express instructions and as necessary to perform services for purposes set forth in this Privacy Notice. The Service Providers will also be required to safeguard the security and confidentiality of the Data they process by implementing appropriate technical and organizational security measures and confidentiality obligations binding employees accessing Data.
+    </p>
+  </li>
+
+  <li className="mb-2">
+    When IRCPL acts as a Service Provider: We may process and share your Data with Tata Group Entities and Partners when we act as a service provider to such Tata Group Entities and Partners.
+  </li>
+
+  <li className="mb-2">
+    Protecting IRCPL: We may release Data when we believe release is appropriate to comply with applicable law or legal process, enforce or apply the Terms of Use of our website or mobile application and other agreements, protect IRCPL against harm or financial loss, when we believe disclosure is necessary to protect individuals’ vital interests, or in connection with an investigation of suspected or actual fraudulent or illegal activity. This may include exchanging information with other companies and organizations for fraud protection, risk management, and dispute resolution. This does not include selling or otherwise disclosing personally identifiable information from users for commercial purposes in violation of this Privacy Notice.
+  </li>
+
+  <li className="mb-2">
+    Business Transfers: As we continue to develop our business, we might sell or buy subsidiaries or business units. Your Data (including in relation to loyalty programs) may be transferred as part of such transaction or in the course of exploratory activities related thereto, including due diligence and audit exercises. The relevant transferee may thereafter make use of Data in accordance with then-applicable Law.
+  </li>
+
+  <li>
+    Third Parties: We may also share your Data with other third parties where:
+    <ol className="list-decimal ml-6 mt-2">
+      <li>You request or authorize us to do so;</li>
+      <li>We need to comply with applicable law or respond to valid legal process; or</li>
+      <li>We need to operate and maintain the security of our website or mobile application, including to prevent or stop an attack on our computer systems or networks.</li>
+    </ol>
+  </li>
+</ol>
+
+
+      </div>
+    </div>
+  );
+};
+
+export default Terms;

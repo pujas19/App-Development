960ff27a5515fff673ae1e3f9a9ef2829905cc46
pujas19 <110535076+pujas19@users.noreply.github.com>
2023-10-05 15:26:07 +0530

    Add files via upload

---
+++ Day11/main/java/com/puja/login/Enum/Role.java
@@ -0,0 +1,9 @@
+package com.puja.login.Enum;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum Role {
+    ADMIN,
+    STUDENT
+}
---
+++ Day11/main/java/com/puja/login/LoginSpringJwtApplication.java
@@ -0,0 +1,13 @@
+package com.puja.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSpringJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginSpringJwtApplication.class, args);
+	}
+
+}
